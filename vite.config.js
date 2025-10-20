2b43625036e017402b881988dc2fabd0df9cdf49
astastu <hello@stuartar.com>
2025-10-20 13:30:10 +1000

    Force v3 bundle to bypass all caches

--- vite.config.js
+++ vite.config.js
@@ -26,9 +26,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
-        assetFileNames: `assets/[name]-[hash]-v2.[ext]`
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
+        assetFileNames: `assets/[name]-[hash]-v3.[ext]`
       },
     },
   },
